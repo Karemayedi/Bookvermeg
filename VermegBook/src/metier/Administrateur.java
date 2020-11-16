bf36ec1742d14e3f8c1244eabe93faa30962c536
ASUS <ASUS@DESKTOP-9EVNMU4>
2020-11-16 09:45:51 +0100

    added class Administrateur.java

---
+++ VermegBook/src/metier/Administrateur.java
@@ -0,0 +1,10 @@
+package metier;
+
+
+public class Administrateur extends Personne {
+
+	public Administrateur(int id, String nom, String prenom) {
+		super(id, nom, prenom);
+		// TODO Auto-generated constructor stub
+	}
+}
